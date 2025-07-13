8b7b33303f33bb2073e968ae9b69e7056fac9166
Tauhid Hassan <tanvirmolla036@gmail.com>
2025-07-13 12:36:21 +0600

    Update setname.js

--- Script/commands/setname.js
+++ Script/commands/setname.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setname",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "Tauhid ☢️_𖣘 -Bot_ ☢️",
 	description: "Change the nickname in your group or the person you tag",
 	commandCategory: "Box Chat",
 	usages: "[name]",
